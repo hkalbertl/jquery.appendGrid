5406993e6ac021992fa4587cf4bf4fd876f094a2
Albert L <hkalbertl@users.noreply.github.com>
2020-08-20 10:32:45 +0800

    Working on v2.0.7
    - Supported bootstrap-icons

---
+++ src/lib/ag-icon-bootstrapicons.js
@@ -0,0 +1,39 @@
+﻿import * as Util from './ag-util';
+import IconBase from './ag-icon-base';
+
+class IconBootstrapIcons extends IconBase {
+    constructor(iconParams) {
+        super('icon-bootstrapicons');
+        // Prepare default options
+        let libParams = {
+            baseUrl: '',
+            icons: null
+        };
+        Object.assign(libParams, iconParams);
+        // Set default CSS class as icon
+        let icons = {
+            append: 'plus',
+            removeLast: 'dash',
+            insert: 'arrow-90deg-left',
+            remove: 'trash',
+            moveUp: 'chevron-up',
+            moveDown: 'chevron-down'
+        };
+        // Override default icons if defined
+        if (libParams.icons) {
+            Object.assign(icons, libParams.icons);
+        }
+        this.icons = icons;
+        this.baseUrl = libParams.baseUrl;
+    }
+
+    generateIcon(container, type) {
+        let icon = document.createElement('img');
+        icon.src = this.baseUrl + this.icons[type] + '.svg';
+        Util.applyClasses(icon, this.icons[type]);
+        container.appendChild(icon);
+        return icon;
+    }
+}
+
+export default IconBootstrapIcons;
