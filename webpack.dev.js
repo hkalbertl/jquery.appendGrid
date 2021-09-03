8dff08a53dac5c381e6fa790a7a5b14e8258ef40
Albert L <hkalbertl@users.noreply.github.com>
2021-09-03 11:39:19 +0800

    Upgraded to Webpack 5

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,26 +3,32 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+
 const webpack = require('webpack');
 
 module.exports = merge(common, {
 	mode: 'development',
 	devtool: 'inline-source-map',
-    devServer: {
-        historyApiFallback: true,
+	devServer: {
+		historyApiFallback: true,
 		port: 9000,
-        open: true
-    },
+		open: true
+	},
 	plugins: [
 		new HTMLWebpackPlugin({
-			template: path.resolve(__dirname, 'src/dev-bootstrap4.html')
+			// template: path.resolve(__dirname, 'src/dev-bootstrap4.html')
+			template: path.resolve(__dirname, 'src/dev-bootstrap5.html')
 			// template: path.resolve(__dirname, 'src/dev-bulma.html')
 			// template: path.resolve(__dirname, 'src/dev-foundation6.html')
 		}),
 		new HtmlWebpackTagsPlugin({
-			tags: 'src/dev-data.js',
-			append: true
-		}),
-		new webpack.HotModuleReplacementPlugin()
+			scripts: [
+				{
+					append: false,
+					sourcePath: path.resolve(__dirname, 'src/dev-data.js'),
+					path: 'dev-data.js'
+				}
+			]
+		})
 	]
 });
